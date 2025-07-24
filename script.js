30e4fc1d0a02cb66b7aa51f6b9120c76c374c229
Narat1111 <Naratoffice@gmail.com>
2025-07-24 21:43:55 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function installIPA() {
-  const manifestURL = "https://yourdomain.com/game.plist"; // 🔁 ប្ដូរជា URL IPA របស់អ្នក
-
-  const confirmInstall = confirm("តើអ្នកចង់ដំឡើង IPA នេះឬទេ?");
-  if (confirmInstall) {
-    window.location.href = `itms-services://?action=download-manifest&url=${manifestURL}`;
-  }
-}
